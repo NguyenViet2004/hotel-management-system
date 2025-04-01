557bb162b32eca2e7090972eb9826c84a32f0d25
PC <PC@MSI>
2025-04-01 16:44:25 +0700

    duong up ne dao

--- LuxClass/src/GUI/DoiPhong_GUI.java
+++ LuxClass/src/GUI/DoiPhong_GUI.java
@@ -6,8 +6,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
