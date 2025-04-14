9352193220cf44baf0048fc7e971713cd63d970a
NguyenViet <nguyenviet21578@gmail.com>
2025-04-14 21:18:48 +0700

    thêm hàm TimDonTheoMa()

---
+++ LuxClass/src/dao/DonDatPhong_Dao.java
@@ -0,0 +1,71 @@
+package dao;
+
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import connectDB.ConnectDB;
+import entity.DonDatPhong;
+import entity.KhachHang;
+import entity.NhanVien;
+
+public class DonDatPhong_Dao {
+	private KhachHang_Dao khachHang_Dao = new KhachHang_Dao();
+	private NhanVien_Dao nhanVien_Dao = new NhanVien_Dao();
+
+	public DonDatPhong timDonTheoMa(String maDonDatPhong) {
+		DonDatPhong don = null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			conn = ConnectDB.getConnection();
+			String sql = "SELECT * FROM DonDatPhong WHERE maDonDatPhong = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, maDonDatPhong);
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				don = new DonDatPhong();
+
+				don.setMaDonDatPhong(rs.getString("maDonDatPhong"));
+
+				// Ngày nhận, trả -> convert sang LocalDateTime
+				Timestamp tsNhan = rs.getTimestamp("ngayNhanPhong");
+				Timestamp tsTra = rs.getTimestamp("ngayTraPhong");
+				if (tsNhan != null)
+					don.setNgayNhanPhong(tsNhan.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+				if (tsTra != null)
+					don.setNgayTraPhong(tsTra.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+
+				don.setSoKhach(rs.getInt("soKhach"));
+				don.setTienCoc(rs.getDouble("tienCoc"));
+				don.setLoaiDon(rs.getString("loaiDon"));
+				don.setTrangThai(rs.getString("trangThai"));
+
+				// Gán thông tin khách hàng và nhân viên từ DAO
+				String maKH = rs.getString("maKH");
+				String maNV = rs.getString("maNV");
+
+				KhachHang kh = khachHang_Dao.timTheoMa(maKH);
+				NhanVien nv = nhanVien_Dao.timTheoMa(maNV);
+
+				don.setKhachHang(kh);
+				don.setNhanVien(nv);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (ps != null) ps.close();
+				if (conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return don;
+	}
+}
