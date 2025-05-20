2f1820147670573bb1b179a568d7f09c39758966
Daole2004 <emailcuadao@gmail.com>
2025-05-20 16:38:18 +0700

    thêm mô tả cho phòng

--- LuxClass/data/Lux_SQL.sql
+++ LuxClass/data/Lux_SQL.sql
@@ -121,31 +121,51 @@ VALUES
     ('double', N'Double Room', 10, 45, 250000, 1000000, 900000, 200000),
     ('triple', N'Triple Room', 6, 50, 300000, 1400000, 1300000, 250000);
 go
-INSERT INTO Phong (soPhong, trangThai, loaiPhong, moTa)
-VALUES 
-    -- Single Room (8 phòng)
-    ('P101', N'Trống', 'single', NULL), ('P102', N'Trống', 'single', NULL),
-    ('P201', N'Trống', 'single', NULL), ('P202', N'Trống', 'single', NULL),
-    ('P301', N'Trống', 'single', NULL), ('P302', N'Trống', 'single', NULL),
-    ('P401', N'Trống', 'single', NULL), ('P402', N'Trống', 'single', NULL),
+INSERT INTO Phong (soPhong, trangThai, loaiPhong, moTa) VALUES
+	-- Tổ hợp 1: Chỉ Ban công
+	('P101', N'Trống', 'single', N'Ban công'),
+	('P201', N'Trống', 'single', N'Ban công'),
+	('P301', N'Trống', 'double', N'Ban công'),
+	('P401', N'Trống', 'triple', N'Ban công'),
 
-    -- Twin Room (7 phòng)
-    ('P103', N'Trống', 'twin', NULL), ('P203', N'Trống', 'twin', NULL),
-    ('P303', N'Trống', 'twin', NULL), ('P403', N'Trống', 'twin', NULL),
-    ('P501', N'Trống', 'twin', NULL), ('P601', N'Trống', 'twin', NULL),
-    ('P602', N'Trống', 'twin', NULL),
+	-- Tổ hợp 2: Chỉ View biển
+	('P102', N'Trống', 'single', N'View biển'),
+	('P202', N'Trống', 'double', N'View biển'),
+	('P302', N'Trống', 'triple', N'View biển'),
+	('P403', N'Trống', 'double', N'View biển'),
 
-    -- Double Room (10 phòng)
-    ('P104', N'Trống', 'double', NULL), ('P204', N'Trống', 'double', NULL),
-    ('P304', N'Trống', 'double', NULL), ('P404', N'Trống', 'double', NULL),
-    ('P502', N'Trống', 'double', NULL), ('P603', N'Trống', 'double', NULL),
-    ('P105', N'Trống', 'double', NULL), ('P205', N'Trống', 'double', NULL),
-    ('P305', N'Trống', 'double', NULL), ('P405', N'Trống', 'double', NULL),
+	-- Tổ hợp 3: Chỉ Hút thuốc
+	('P103', N'Trống', 'single', N'Phòng hút thuốc'),
+	('P203', N'Trống', 'twin', N'Phòng hút thuốc'),
+	('P303', N'Trống', 'double', N'Phòng hút thuốc'),
+	('P402', N'Trống', 'triple', N'Phòng hút thuốc'),
 
-    -- Triple Room (6 phòng)
-    ('P106', N'Trống', 'triple', NULL), ('P206', N'Trống', 'triple', NULL),
-    ('P306', N'Trống', 'triple', NULL), ('P406', N'Trống', 'triple', NULL),
-    ('P503', N'Trống', 'triple', NULL), ('P504', N'Trống', 'triple', NULL);
+	-- Tổ hợp 4: Ban công + View biển
+	('P104', N'Trống', 'double', N'Ban công, View biển'),
+	('P204', N'Trống', 'twin', N'Ban công, View biển'),
+	('P304', N'Trống', 'triple', N'Ban công, View biển'),
+	('P404', N'Trống', 'double', N'Ban công, View biển'),
+
+	-- Tổ hợp 5: Ban công + Hút thuốc
+	('P105', N'Trống', 'double', N'Ban công, Phòng hút thuốc'),
+	('P205', N'Trống', 'single', N'Ban công, Phòng hút thuốc'),
+	('P305', N'Trống', 'triple', N'Ban công, Phòng hút thuốc'),
+	('P405', N'Trống', 'twin', N'Ban công, Phòng hút thuốc'),
+
+	-- Tổ hợp 6: View biển + Hút thuốc
+	('P106', N'Trống', 'double', N'View biển, Phòng hút thuốc'),
+	('P206', N'Trống', 'twin', N'View biển, Phòng hút thuốc'),
+	('P306', N'Trống', 'single', N'View biển, Phòng hút thuốc'),
+	('P406', N'Trống', 'double', N'View biển, Phòng hút thuốc'),
+
+	-- Tổ hợp 7: Ban công + View biển + Hút thuốc
+	('P501', N'Trống', 'twin', N'Ban công, View biển, Phòng hút thuốc'),
+	('P502', N'Trống', 'double', N'Ban công, View biển, Phòng hút thuốc'),
+	('P503', N'Trống', 'triple', N'Ban công, View biển, Phòng hút thuốc'),
+	('P504', N'Trống', 'twin', N'Ban công, View biển, Phòng hút thuốc'),
+	('P601', N'Trống', 'twin', N'Ban công, View biển, Phòng hút thuốc'),
+	('P602', N'Trống', 'double', N'Ban công, View biển, Phòng hút thuốc'),
+	('P603', N'Trống', 'single', N'Ban công, View biển, Phòng hút thuốc');
 go
 INSERT INTO NhanVien (maNV, hoTen, ngaySinh, sdt, diaChi, soCCCD, chucVu, caLamViec)
 VALUES 
