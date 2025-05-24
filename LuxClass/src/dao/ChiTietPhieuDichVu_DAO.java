44e642a22b828fff82bbad0092d4df5802c7e2a3
Daole2004 <emailcuadao@gmail.com>
2025-05-25 03:19:33 +0700

    thêm hàm thêmchitietphieudichvu ở daophieudichvudao

--- LuxClass/src/dao/ChiTietPhieuDichVu_DAO.java
+++ LuxClass/src/dao/ChiTietPhieuDichVu_DAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import connectDB_CuaXien.ConnectDB;
+import entity.ChiTietPhieuDichVu;
 
 public class ChiTietPhieuDichVu_DAO {
 
@@ -23,6 +24,23 @@ public class ChiTietPhieuDichVu_DAO {
         } catch (SQLException e) {
             e.printStackTrace();
             return false; // Xóa thất bại
+        }
+    }
+    public boolean them(ChiTietPhieuDichVu ctpdv) {
+        String sql = "INSERT INTO ChiTietPhieuDichVu (maPhieuDichVu, maDichVu, soLuong) VALUES (?, ?, ?)";
+        try (Connection conn = ConnectDB.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, ctpdv.getPhieuDichVu().getMaPhieuDichVu());
+            stmt.setString(2, ctpdv.getDichVu().getMaDV());
+            stmt.setInt(3, ctpdv.getSoLuong());
+
+            int rowsInserted = stmt.executeUpdate();
+            return rowsInserted > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
